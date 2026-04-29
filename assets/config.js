17e42e977ff04edd94f50618b4ba0dfbe86bb096
kneck99 <kneck.n.o.d@gmail.com>
2026-04-29 09:23:43 +0900

    Update config.js

--- assets/config.js
+++ assets/config.js
@@ -1,6 +1,6 @@
 window.HS_CONFIG = {
   siteTitle: '한살림 농업살림센터 예약',
   apiBaseUrl: 'https://reservation-hansalimnc.chopsquid.workers.dev',
-  appsScriptUrl: 'https://script.google.com/macros/s/YOUR_DEPLOYMENT_ID/exec',
+  appsScriptUrl: 'https://script.google.com/macros/s/AKfycbzYNboXx69-QRESKrRmU71cVFrp4jCS98slWTq0HQ3SOZ8tmQe6tXRhxpMoHUNoxW_v-A/exec',
   homepageUrl: 'https://www.hansalimnc.co.kr'
 };
